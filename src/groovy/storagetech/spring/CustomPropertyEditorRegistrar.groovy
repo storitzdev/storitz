b38effe8e324b4361e52c4bf0421f4115c5af227
Matthew Amster <mamster@storitz.com>
2010-06-27 22:13:16 +0000

    Fixed path

---
+++ src/groovy/storagetech/spring/CustomPropertyEditorRegistrar.groovy
@@ -0,0 +1,24 @@
+package storitz.spring
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mamster
+ * Date: Jun 27, 2010
+ * Time: 11:29:02 AM
+ * To change this template use File | Settings | File Templates.
+ */
+import java.util.Date
+import java.text.SimpleDateFormat
+import org.springframework.beans.propertyeditors.CustomDateEditor
+import org.springframework.beans.PropertyEditorRegistrar
+import org.springframework.beans.PropertyEditorRegistry
+
+public class CustomPropertyEditorRegistrar implements PropertyEditorRegistrar {
+  public void registerCustomEditors(PropertyEditorRegistry registry) {
+    registry.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("dd-MM-yyyy"), true));
+   }
+}
+
+
+
+
