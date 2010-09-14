1bdc21f951aea5477a47ecdac41f2f637b2906e6
Matthew Amster <mamster@storitz.com>
2010-09-14 00:48:36 +0000

    More work on CShift 4 service

--- grails-app/services/storitz/CShift4Service.groovy
+++ grails-app/services/storitz/CShift4Service.groovy
@@ -1,6 +1,14 @@
 package storitz
 import groovyx.net.ws.WSClient
 import com.storitz.CenterShift
+import com.storitz.StorageSite
+import storitz.constants.State
+import com.storitz.Role
+import com.storitz.UserRole
+import com.storitz.UserNotificationType
+import com.storitz.NotificationType
+import com.storitz.SiteUser
+import com.storitz.User
 
 class CShift4Service {
 
@@ -26,6 +34,33 @@ class CShift4Service {
       return lookupUser
     }
 
+    def createSiteUser(site, email, realName, manager) {
+      def user = User.findByEmail(email)
+      if (!user) {
+        user = new User(
+          username:email,
+          password: (Math.random() * System.currentTimeMillis()) as String,
+          description: "Site Manager for ${site.title}",
+          email: email,
+          userRealName:realName,
+          accountExpired: false,
+          accountLocked: false,
+          passwordExpired: false,
+          enabled: false
+        )
+        user.manager = manager
+        user.save(flush: true)
+        SiteUser.link(site, user)
+      }
+      if (!UserNotificationType.userHasNotificationType(user, 'NOTIFICATION_SITE_MANAGER')) {
+        def notificationType = NotificationType.findByNotificationType('NOTIFICATION_SITE_MANAGER')
+        UserNotificationType.create(user, notificationType, true)
+      }
+      if (!UserRole.userHasRole(user,'ROLE_USER')) {
+        UserRole.create(user, Role.findByAuthority('ROLE_USER'), true)
+      }
+    }
+
     def loadSites(cshift, stats) {
       getProxy()
       def siteListRequest = proxy.create("com.centershift.store40.GetSiteListRequest")
@@ -35,7 +70,42 @@ class CShift4Service {
       def siteList = proxy.GetSiteList(lookupUser, siteListRequest)
 
       for (site in siteList.details.soagetsitelist) {
-        println "retrieved site: ${site.dump()}"
+        StorageSite foundSite = StorageSite.findBySourceAndSourceId("CS4", site.siteid)
+        if (foundSite) {
+          // TODO - handle update case
+        } else {
+          if (site.propertytype == 1 && (site.sitestatus == 1 | site.sitestatus == 2)) {
+
+            def csite = new StorageSite()
+            csite.sourceId = site.siteid
+            csite.sourceLoc = site.sitenumber
+            csite.title = site.displayname
+            csite.disabled = true
+            csite.centerShift = cshift
+            csite.siteLink = null
+
+            csite.phone = site.phone
+            csite.address = site.line1
+            csite.address2 = site.line2
+            csite.city = site.city
+            csite.state = State.fromText(site.state)
+            csite.zipcode = site.postalcode
+
+            site.save()
+            SiteUser.link(csite, cshift.manager)
+            if (site.email?.size() > 0) {
+              createSiteUser(site, site.email, site.email, cshift.manager)
+            }
+
+            // TODO - promos, insurance, hours, site features
+
+            site.save(flush:true)
+
+          } else {
+            println "Skipped site ${site.displayname} due to status ${site.sitestatus} or property type ${site.propertytype}"
+          }
+        }
+
       }
     }
 
