be66b09db3093221bd1e28a7b073388a90c7c508
Matthew Amster <mamster@storitz.com>
2010-09-17 16:28:47 +0000

    SEO changes

--- grails-app/controllers/storitz/HomeController.groovy
+++ grails-app/controllers/storitz/HomeController.groovy
@@ -5,6 +5,6 @@ import com.storitz.StorageSize
 class HomeController {
 
     def index = {
-      [ sizeList: StorageSize.list(params), title:"Click. Store. Done."]
+      [ sizeList: StorageSize.list(params), title:"Storitz online self storage rental. Click. Store. Done."]
     }
 }
