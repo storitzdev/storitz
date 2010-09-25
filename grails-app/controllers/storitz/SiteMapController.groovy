a5c23f75d46203b76c04bb0aa32aea6e922d97e8
Matthew Amster <mamster@storitz.com>
2010-09-25 15:51:03 +0000

    Sitemap work

--- grails-app/controllers/storitz/SiteMapController.groovy
+++ grails-app/controllers/storitz/SiteMapController.groovy
@@ -8,14 +8,15 @@ class SiteMapController
   def index = {
     withFormat {
       html {
-        def sites = StorageSite.findAllByDisabled(false, [sort:"city"]).groupBy { StorageSite site -> site.stateName }
-
+        //def sites = StorageSite.findAllByDisabled(false, [sort:"city"]).groupBy { StorageSite site -> site.stateName }
+        def sites = []
         // Converting to TreeMap so keys (state names) will iterate in alphabetical order.
         [storageSiteStateMap: sites as TreeMap]
       }
 
       xml {
-        def siteList = StorageSite.findAllByDisabled(false)
+        //def siteList = StorageSite.findAllByDisabled(false)
+        def siteList = []
         render(view:"sitemap", contentType:'text/xml; charset=utf-8', model:[siteList:siteList])
       }
     }
