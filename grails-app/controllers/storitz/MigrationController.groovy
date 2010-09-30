2b6e2b016a92dba8e9d1a837534b5820ca212688
Matthew Amster <mamster@storitz.com>
2010-09-30 01:46:29 +0000

    more migration changes

--- grails-app/controllers/storitz/MigrationController.groovy
+++ grails-app/controllers/storitz/MigrationController.groovy
@@ -16,4 +16,26 @@ class MigrationController {
       render(status: 200, contentType: "application/json", text: "{ \"sites\": ${sites as JSON} }")
     }
   }
+
+  def consume = {
+    def urlString = 'http://preview.storitz.com/migrate/owner/' + params.id
+    if (params.url) {
+      urlString = params.url
+    }
+
+    println "URL = ${urlString}"
+
+    def url = new URL(urlString)
+    def conn = url.openConnection()
+    if (conn.responseCode == 200) {
+      def sites = JSON.parse(conn.content.text)
+      for (s in sites) {
+        def  site = new StorageSite(s)
+        println "Retrieved site: ${site.title} id = ${site.id}"
+      }
+    } else {
+      println "Bad connection got response ${conn.responseCode}"
+    }
+    render(status: 200, text: "Done!")
+  }
 }
