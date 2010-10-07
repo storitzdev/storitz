8ac214985be5f6393cda568242c1f7781bc8ea40
Matthew Amster <mamster@storitz.com>
2010-10-07 01:46:55 +0000

    Added new mechanism for metro area landing pages

---
+++ grails-app/domain/com/storitz/Metro.groovy
@@ -0,0 +1,15 @@
+package com.storitz
+
+import storitz.constants.State
+
+class Metro {
+
+  static constraints = {
+    note(widget:'textarea', nullable:true, size:2..3000, markup:true)
+  }
+
+  String note
+  String city
+  State state
+
+}
