c33ec3c9e3996a907ade52bcacc5a53ec9c1e18d
Matthew Amster <mamster@storitz.com>
2010-10-27 19:08:02 +0000

    Added ant dependency for build

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,13 @@
+grails.project.dependency.resolution = {
+  inherits( "global" )
+  log "warn"
+  repositories {
+    grailsPlugins()
+    grailsHome()
+    grailsCentral()
+  }
+  dependencies {
+    runtime "org.apache.ant:ant:1.7.1"
+    runtime "org.apache.ant:ant-launcher:1.7.1"
+  }
+}
