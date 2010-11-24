57d4d345ac8f7339c11f0b9fd4d5e78b4949d5e2
Matthew Amster <mamster@storitz.com>
2010-11-24 08:46:34 +0000

    Handle null info Fix error page

--- grails-app/controllers/storitz/HomeController.groovy
+++ grails-app/controllers/storitz/HomeController.groovy
@@ -156,12 +156,12 @@ class HomeController {
       }
       if (site.featuredOffers().size() == 0) {
         def totals = costService.calculateTotals(site, bestUnit, null, null, moveInDate)
-        siteMoveInPrice[site.id] = [cost:totals['moveInTotal'], promo:null, promoName:null, monthly:bestUnit?.pushRate, paidThruDate:totals['paidThruDate'], sizeDescription: bestUnit?.unitsize.description, unitType:bestUnit?.unitType.display]
+        siteMoveInPrice[site.id] = [cost:totals['moveInTotal'], promo:null, promoName:null, monthly:bestUnit?.pushRate, paidThruDate:totals['paidThruDate'], sizeDescription: bestUnit?.unitsize?.description, unitType:bestUnit?.unitType?.display]
       } else {
         for (promo in site.featuredOffers()) {
           def totals = costService.calculateTotals(site, bestUnit, promo, null, moveInDate)
           if (!siteMoveInPrice[site.id] || siteMoveInPrice[site.id].cost > totals['cost']) {
-            siteMoveInPrice[site.id] = [cost:totals['moveInTotal'], promo:promo.id, promoName:promo.promoName, monthly:bestUnit.pushRate, paidThruDate:totals['paidThruDate'], sizeDescription: bestUnit.unitsize.description, unitType:bestUnit?.unitType.display]
+            siteMoveInPrice[site.id] = [cost:totals['moveInTotal'], promo:promo.id, promoName:promo.promoName, monthly:bestUnit?.pushRate, paidThruDate:totals['paidThruDate'], sizeDescription: bestUnit?.unitsize?.description, unitType:bestUnit?.unitType?.display]
           }
         }
       }
