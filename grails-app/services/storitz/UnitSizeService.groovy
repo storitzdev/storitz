0c64bf126e78f6e71877a7c4414cc6c643312a59
Matthew Amster <mamster@storitz.com>
2011-01-12 22:03:53 +0000

    Change rules to length based

--- grails-app/services/storitz/UnitSizeService.groovy
+++ grails-app/services/storitz/UnitSizeService.groovy
@@ -8,12 +8,19 @@ class UnitSizeService {
     boolean transactional = false
 
     def getUnitSize(width, length, SearchType searchType) {
-      def unitSizes = StorageSize.findAllByWidthAndLength(width, length)
-      def unitSize = unitSizes.find{it.searchType == searchType}
+      def unitSize
+      if (searchType == SearchType.STORAGE) {
+        def unitSizes = StorageSize.findAllByWidthAndLength(width, length)
+        unitSize = unitSizes.find{it.searchType == searchType}
+      }
       if (unitSize == null) {
 
         def unitArea = width * length
-        unitSize = StorageSize.findAllBySearchType(searchType).findAll { it.length * it.width <= unitArea}?.min{ unitArea - it.width * it.length }
+        if (searchType == SearchType.STORAGE) {
+          unitSize = StorageSize.findAllBySearchType(searchType).findAll { it.length * it.width <= unitArea}?.min{ unitArea - it.width * it.length }
+        } else if (searchType == SearchType.PARKING) {
+          unitSize = StorageSize.findAllBySearchType(searchType).findAll { it.length >= length }.min{ it.length - length }
+        }
       }
       return unitSize
     }
