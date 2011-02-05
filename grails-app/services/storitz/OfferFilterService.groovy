edc44db590d94d8d6483653b05fb21449e249efc
Matthew Amster <mamster@storitz.com>
2011-02-05 01:07:52 +0000

    Remove debugging from offer filter

--- grails-app/services/storitz/OfferFilterService.groovy
+++ grails-app/services/storitz/OfferFilterService.groovy
@@ -37,17 +37,17 @@ class OfferFilterService {
           def validUnitSize = (offer.restrictions.findAll{it.type == SpecialOfferRestrictionType.UNIT_SIZE}.size() == 0)
           def validOccupancyRate = (offer.restrictions.findAll{it.type == SpecialOfferRestrictionType.OCCUPANCY_RATE}.size() == 0)
           def validMinimumAvailable = (offer.restrictions.findAll{it.type == SpecialOfferRestrictionType.MINIMUM_AVAILABLE}.size() == 0)
-          println "In offer filter validUnitType=${validUnitType}, validUnitSize=${validUnitSize}, validOccupancyRate=${validOccupancyRate}, validMinimumAvailable=${validMinimumAvailable}"
+          //println "In offer filter validUnitType=${validUnitType}, validUnitSize=${validUnitSize}, validOccupancyRate=${validOccupancyRate}, validMinimumAvailable=${validMinimumAvailable}"
           for(restriction in offer.restrictions) {
             switch(restriction.type) {
               case SpecialOfferRestrictionType.UNIT_TYPE:
-                println "UnitType filter ${unit.unitTypeInfo} restriction ${restriction.restrictionInfo}"
+                //println "UnitType filter ${unit.unitTypeInfo} restriction ${restriction.restrictionInfo}"
                 if (unit.unitTypeInfo == restriction.restrictionInfo) {
                   validUnitType = true
                 }
                 break
               case SpecialOfferRestrictionType.UNIT_SIZE:
-                println "UnitSize filter ${unit.unitSizeInfo} restriction ${restriction.restrictionInfo}"                
+                //println "UnitSize filter ${unit.unitSizeInfo} restriction ${restriction.restrictionInfo}"
                 if (unit.unitSizeInfo == restriction.restrictionInfo) {
                   validUnitSize = true
                 }
@@ -58,7 +58,7 @@ class OfferFilterService {
                   unitCount = site.units.findAll{ it.unitTypeInfo == unit.unitTypeInfo}.size()
                 }
                 BigDecimal currentRate = ((unitCount * 100) / unit.totalUnits)
-                println "Occupancy rate filter ${restriction.minRange} <= ${currentRate} <= ${restriction.maxRange}"                
+                //println "Occupancy rate filter ${restriction.minRange} <= ${currentRate} <= ${restriction.maxRange}"
                 if (restriction.minRange <= currentRate && currentRate <= restriction.maxRange) {
                   validOccupancyRate = true
                 }
@@ -68,16 +68,16 @@ class OfferFilterService {
                 if (site.feed.feedType == FeedType.SITELINK) {
                   unitCount = site.units.findAll{ it.unitTypeInfo == unit.unitTypeInfo}.size()
                 }
-                println "Minimum available filter ${restriction.minRange} <= ${unitCount} <= ${restriction.maxRange}"                
+                //println "Minimum available filter ${restriction.minRange} <= ${unitCount} <= ${restriction.maxRange}"
                 if (restriction.minRange <= unitCount && unitCount <= restriction.maxRange) {
                   validMinimumAvailable = true
                 }
                 break
             }
           }
-          println "Leaving filter validUnitType=${validUnitType}, validUnitSize=${validUnitSize}, validOccupancyRate=${validOccupancyRate}, validMinimumAvailable=${validMinimumAvailable}"
+          //println "Leaving filter validUnitType=${validUnitType}, validUnitSize=${validUnitSize}, validOccupancyRate=${validOccupancyRate}, validMinimumAvailable=${validMinimumAvailable}"
           if (!(validUnitType && validUnitSize && validOccupancyRate && validMinimumAvailable)) {
-            println "Special Offer ${offer.promoName} removed"
+            //println "Special Offer ${offer.promoName} removed"
             iter.remove()
           }
         }
