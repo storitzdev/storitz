b040cd54758a0d8fd00978186a217f56ce274286
Charlie <charlie@storitz.com>
2011-07-10 19:37:23 +0000

    Fixing migration (which must be run using 'grails run-script scripts/UpdateSeoCitiesForSites.groovy')

--- scripts/UpdateSeoCitiesForSites.groovy
+++ scripts/UpdateSeoCitiesForSites.groovy
@@ -4,11 +4,13 @@ import com.storitz.StorageSite
 import com.storitz.StoritzUtil
 
 try {
-    def storageSites = StorageSite.findAll() { it.city == "San Diego" };
+    def storageSites = StorageSite.findAllByDisabled(0);
     println "  -> Found ${storageSites.size()} sites"
     storageSites.each { site ->
-        site.seoCity = StoritzUtil.seoEncode(site.city); // TODO: is this even necessary? Should be done automatically by beforeUpdate when we call save?
-        site.save(flush: true)
+        if (site.seoCity == null) {
+          site.seoCity = StoritzUtil.seoEncode(site.city); // TODO: is this even necessary? Should be done automatically by beforeUpdate when we call save?
+          site.save(flush: true)
+        }
     }
     println "Job complete."
 }
