57665302cef9b76094ee412835ebc9d92ff26dc8
Jason Meade <jmeade@storitz.com>
2011-07-13 21:28:21 +0000

    fixed edomico tests...

--- test/integration/storitz/EDomicoTests.groovy
+++ test/integration/storitz/EDomicoTests.groovy
@@ -27,28 +27,28 @@ class EDomicoTests extends GroovyTestCase {
 //        assertTrue(site != null && site == "BSP")
 //    }
 //
-//    void testGetSizes() {
-//        EDomicoStorageFeedService service = new EDomicoStorageFeedService(443,"uBvQoE0DtVTMQA8xCv57A3Rw")
-//        def token = service.readToken()
-//        def site = service.readSiteID(token,"2227 SAN PABLO AVENUE","OAKLAND","94612")
-//        def sizes = service.readSizes(token,site)
-//
-//        for (int i = 0; i < sizes.size(); i++) {
-//            def sz = sizes[i]
-//            System.out.println("")
-//            System.out.println("SiteID: " + sz.get("SiteID"))
-//            System.out.println("SizeID: " + sz.get("SizeID"))
-//            System.out.println("SizeRentRateReservationDeposit: " + sz.get("SizeRentRateReservationDeposit"))
-//            System.out.println("SizeRentRateSecurityDeposit: " + sz.get("SizeRentRateSecurityDeposit"))
-//            System.out.println("SizeCodeInfo: " + sz.get("SizeCodeInfo"))
-//            System.out.println("SizeRentRate: " + sz.get("SizeRentRate"))
-//            System.out.println("DiscountName: " + sz.get("DiscountName"))
-//            System.out.println("DiscountValue: " + sz.get("DiscountValue"))
-//            System.out.println("IsPercent: " + sz.get("IsPercent"))
-//        }
-//
-//        assertTrue((sizes != null) && (sizes.size() > 0))
-//    }
+    void testGetSizes() {
+        EDomicoStorageFeedService service = new EDomicoStorageFeedService(400,"Oukm8#Wn36K?7Xx/bKf0IgW!Tm7")
+        def token = service.readToken()
+        def site = service.readSiteID(token,"21W379 Lake Street","Addison","60101")
+        def sizes = service.readSizes(token,site)
+
+        for (int i = 0; i < sizes.size(); i++) {
+            def sz = sizes[i]
+            System.out.println("")
+            System.out.println("SiteID: " + sz.get("SiteID"))
+            System.out.println("SizeID: " + sz.get("SizeID"))
+            System.out.println("SizeRentRateReservationDeposit: " + sz.get("SizeRentRateReservationDeposit"))
+            System.out.println("SizeRentRateSecurityDeposit: " + sz.get("SizeRentRateSecurityDeposit"))
+            System.out.println("SizeCodeInfo: " + sz.get("SizeCodeInfo"))
+            System.out.println("SizeRentRate: " + sz.get("SizeRentRate"))
+            System.out.println("DiscountName: " + sz.get("DiscountName"))
+            System.out.println("DiscountValue: " + sz.get("DiscountValue"))
+            System.out.println("IsPercent: " + sz.get("IsPercent"))
+        }
+
+        assertTrue((sizes != null) && (sizes.size() > 0))
+    }
 //
 //    void testGetUnitId() {
 //        EDomicoStorageFeedService service = new EDomicoStorageFeedService(443,"uBvQoE0DtVTMQA8xCv57A3Rw")
@@ -102,42 +102,41 @@ class EDomicoTests extends GroovyTestCase {
 //        assertTrue(count > 0)
 //    }
 
-    void testLoadFeeds() {
-        def feeds = com.storitz.EDomico.findAll()
+     void testLoadFeeds() {
+         def feeds = com.storitz.EDomico.findAll()
 
-        for (int i = 0; i < feeds.size(); i++) {
-            def feed = feeds[i]
-            System.out.println("${feed.edomicoClientID}, ${feed.edomicoWebServicesKey}")
-            EDomicoStorageFeedService service = new EDomicoStorageFeedService(feed.edomicoClientID.intValue(),feed.edomicoWebServicesKey)
-            def token = service.readToken()
-            for (int j = 0; j < feed.locations.size(); j++) {
-                def loc = feed.locations[j]
-                def site  = service.readSiteID(token,loc.address1,loc.city,loc.zipcode)
-                def sizes = service.readSizes(token,site)
-                for (int k = 0; k < sizes.size(); k++) {
-                    def sz = sizes[k]
-                    def sizeID = new Integer(sz.get("SizeID")).intValue()
-                    def units = service.readUnits(token,site,sizeID)
-                    def count = units.size()
-                    System.out.println("============================================================================")
-                    System.out.println("FEED: ${feed.edomicoClientID}/${feed.edomicoWebServicesKey} [ID=${feed.id}]")
-                    System.out.println("LOCATION: ${loc.siteID}/${loc.siteName} [ID=${loc.id}]")
-                    System.out.println("          ${loc.address1}")
-                    System.out.println("          ${loc.city}, ${loc.zipcode}")
-                    System.out.println("SiteID: " + sz.get("SiteID"))
-                    System.out.println("SizeID: " + sz.get("SizeID") + " [COUNT=${count}]")
-                    System.out.println("SizeRentRateReservationDeposit: " + sz.get("SizeRentRateReservationDeposit"))
-                    System.out.println("SizeRentRateSecurityDeposit: " + sz.get("SizeRentRateSecurityDeposit"))
-                    System.out.println("SizeCodeInfo: " + sz.get("SizeCodeInfo"))
-                    System.out.println("SizeRentRate: " + sz.get("SizeRentRate"))
-                    System.out.println("DiscountName: " + sz.get("DiscountName"))
-                    System.out.println("DiscountValue: " + sz.get("DiscountValue"))
-                    System.out.println("IsPercent: " + sz.get("IsPercent"))
+         for (int i = 0; i < feeds.size(); i++) {
+             def feed = feeds[i]
+             System.out.println("${feed.edomicoClientID}, ${feed.edomicoWebServicesKey}")
+             EDomicoStorageFeedService service = new EDomicoStorageFeedService(feed.edomicoClientID.intValue(),feed.edomicoWebServicesKey)
+             def token = service.readToken()
+             for (loc in feed.locations) {
+                 def site  = service.readSiteID(token,loc.address1,loc.city,loc.zipcode)
+                 def sizes = service.readSizes(token,site)
+                 for (int k = 0; k < sizes.size(); k++) {
+                     def sz = sizes[k]
+                     def sizeID = new Integer(sz.get("SizeID")).intValue()
+                     def units = service.readUnits(token,site,sizeID)
+                     def count = units.size()
+                     System.out.println("============================================================================")
+                     System.out.println("FEED: ${feed.edomicoClientID}/${feed.edomicoWebServicesKey} [ID=${feed.id}]")
+                     System.out.println("LOCATION: ${loc.siteID}/${loc.siteName} [ID=${loc.id}]")
+                     System.out.println("          ${loc.address1}")
+                     System.out.println("          ${loc.city}, ${loc.zipcode}")
+                     System.out.println("SiteID: " + sz.get("SiteID"))
+                     System.out.println("SizeID: " + sz.get("SizeID") + " [COUNT=${count}]")
+                     System.out.println("SizeRentRateReservationDeposit: " + sz.get("SizeRentRateReservationDeposit"))
+                     System.out.println("SizeRentRateSecurityDeposit: " + sz.get("SizeRentRateSecurityDeposit"))
+                     System.out.println("SizeCodeInfo: " + sz.get("SizeCodeInfo"))
+                     System.out.println("SizeRentRate: " + sz.get("SizeRentRate"))
+                     System.out.println("DiscountName: " + sz.get("DiscountName"))
+                     System.out.println("DiscountValue: " + sz.get("DiscountValue"))
+                     System.out.println("IsPercent: " + sz.get("IsPercent"))
 
-                }
-            }
-        }
+                 }
+             }
+         }
 
-        assertTrue(true)
-    }
+         assertTrue(true)
+     }
 }
