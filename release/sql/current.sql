d86b4085ce15c01f21799c63e72b07c1233d0ae8
Jason Meade <jmeade@storitz.com>
2011-07-13 18:50:16 +0000

    Added release folder to V2 to handle SQL domain changes. Note: Merging this from trunk will cause conflicts. Just grab everything from both versions and everything will be alright.

---
+++ release/sql/current.sql
@@ -0,0 +1,28 @@
+/************************
+ This file contains database schema changes required for deployment
+
+ USAGE:
+
+ 1) Create whatever scripts are required for your changes and save into this
+    file (release/sql/current.sql)
+
+ 2) Update the `releases` table with information specific to your change.
+
+ 3) After your release, copy the contents of this file into a new file
+    (release/sql/historic/release-N.sql) for reference
+ ************************/
+
+START TRANSACTION;
+
+/************************/
+/* ADD YOUR EDITS BELOW */
+/************************/
+
+/* JM: This merge will cause conflicts. Make sure you get everything from both versions. */
+alter table storage_site add is_twenty_four_hour bit(1) null;
+
+/************************/
+/* ADD YOUR EDITS ABOVE */
+/************************/
+
+COMMIT;
