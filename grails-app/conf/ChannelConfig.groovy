fbf1dfe0a15856f6db33293befed4bc4854241ec
Jason Meade <jmeade@storitz.com>
2011-07-19 01:27:31 +0000

    force thank-you to be secure, like its mommy.

--- grails-app/conf/ChannelConfig.groovy
+++ grails-app/conf/ChannelConfig.groovy
@@ -18,7 +18,6 @@ class ChannelConfig {
     requestMap.put new RequestKey("/storageSite/autocompleteSite/**"), [new SecurityConfig("ANY_CHANNEL")]
     requestMap.put new RequestKey("/user/autocompleteUser/**"), [new SecurityConfig("ANY_CHANNEL")]
     requestMap.put new RequestKey("/rentalTransaction/ajax*/**"), [new SecurityConfig("ANY_CHANNEL")]
-    requestMap.put new RequestKey("/thank-you/**"), [new SecurityConfig("ANY_CHANNEL")]
 
     // resources that must be served over https
     requestMap.put new RequestKey("/auth/**"), [new SecurityConfig("REQUIRES_SECURE_CHANNEL")]
@@ -30,6 +29,7 @@ class ChannelConfig {
     requestMap.put new RequestKey("/rentalTransaction/payment/**"), [new SecurityConfig("REQUIRES_SECURE_CHANNEL")]
     requestMap.put new RequestKey("/rentalTransaction/pay/**"), [new SecurityConfig("REQUIRES_SECURE_CHANNEL")]
     requestMap.put new RequestKey("/commission/**"), [new SecurityConfig("REQUIRES_SECURE_CHANNEL")]
+    requestMap.put new RequestKey("/thank-you/**"), [new SecurityConfig("REQUIRES_SECURE_CHANNEL")]
     requestMap
   }
 }
