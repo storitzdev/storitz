ee3e06987e9c20848053a98b4d1d0c3639bc0173
Jason Meade <jmeade@storitz.com>
2011-07-27 16:26:36 +0000

    merging current.sql changes trunk->v2

--- release/sql/current.sql
+++ release/sql/current.sql
@@ -18,10 +18,25 @@ START TRANSACTION;
 /* ADD YOUR EDITS BELOW */
 /************************/
 
-/* JM: This merge will cause conflicts. Make sure you get everything from both versions. */
+/* Create release table */
+CREATE TABLE `releases` (
+  `id` int(11) NOT NULL auto_increment,
+  `revision` int(11) NOT NULL unique,
+  `release_date` datetime NOT NULL,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/* Release 1000: Changes to feed in support of new Uncle Bob's feed type */
+alter table feed add column  `ubcompany_name` varchar(255) default NULL;
 alter table storage_site add is_twenty_four_hour bit(1) null;
 alter table rental_transaction add xid varchar(255) null;
 
+insert into service_map (service_handler,service_name) values ('storitz.UncleBobsStorageFeedService','BOB');
+
+/* update the releases table last */
+insert into releases (revision, release_date,description)
+            values (1000,curdate(),'Uncle Bobs inventory load');
 
 /************************/
 /* ADD YOUR EDITS ABOVE */
