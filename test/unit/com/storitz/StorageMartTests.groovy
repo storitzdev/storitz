eed5727d10d187c631e2151f8715ce896d49f6de
Jason Meade <jmeade@storitz.com>
2011-08-10 22:27:51 +0000

    Storage Mart tests

---
+++ test/unit/com/storitz/StorageMartTests.groovy
@@ -0,0 +1,17 @@
+package com.storitz
+
+import grails.test.*
+
+class StorageMartTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
